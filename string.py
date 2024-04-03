266d157528f0f64d932db7750b0bffac81eb3383
saraswathi kondapalli <146008262+Saraswathi0@users.noreply.github.com>
2024-04-03 19:04:17 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+name = input("")  # taking the input as string
+l1 = ['a', 'e', 'i', 'o', 'u']
+len = len(name)
+name = name.lower()
+v = 0
+c = 0  # initialize the vowels and consonants counts
+
+def count_vowels_and_consonants(name):
+    global v, c                                     #function defintion
+    for i in range(len):
+        if name[i] in l1:
+            v += 1
+        else:
+            c += 1
+    r = {"vowels": v, "consonants": c}
+    return r
+
+print(count_vowels_and_consonants(name))  #function calling 
