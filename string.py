726d0954b72f3adb919c1e557dc49dd066b17122
saraswathi kondapalli <146008262+Saraswathi0@users.noreply.github.com>
2024-04-03 18:36:12 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,13 +1,13 @@
-name=input("")
+name=input("")                   #taking the input as string
 l1=['a','e','i','o','u']
 len=len(name)
 name=name.lower()
 v=0
-c=0
+c=0                  #initialize the vowels and  consonants counts
 for i in range(len):
     if name[i]in l1:
         v=v+1
     else:
         c=c+1
-r={"vowels":v,"constant":c}
+r={"vowels":v," consonants ":c}    
 print(r)
