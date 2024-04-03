4fe1349327d12f8514c05cbd62295c14aa8f5c06
saraswathi kondapalli <146008262+Saraswathi0@users.noreply.github.com>
2024-04-03 18:09:37 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+name=input("")
+l1=['a','e','i','o','u']
+len=len(name)
+name=name.lower()
+v=0
+c=0
+for i in range(len):
+    if name[i]in l1:
+        v=v+1
+    else:
+        c=c+1
+print("vowels=",v)
+print("constant=",c)
